a21d67ab178278b72c72e0706e2f3e049fc9370a
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-02 15:00:44 +0100

    added function construct

--- index.php
+++ index.php
@@ -6,4 +6,12 @@ class Movie
     public $genre;
     public $language;
     public $rank;
+
+    function __construct($title, $genre, $language, $rank)
+    {
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->language = $language;
+        $this->rank = $rank;
+    }
 }
