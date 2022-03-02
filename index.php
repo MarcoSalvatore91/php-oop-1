08bd7078abc3dc657ba15aca89cb7c2244ed7d37
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-02 15:48:06 +0100

    echo all proprietes in class Movie

--- index.php
+++ index.php
@@ -14,4 +14,24 @@ class Movie
         $this->language = $language;
         $this->rank = $rank;
     }
+
+    function all_print($movies)
+    {
+        foreach ($movies as $movie) {
+            echo $movie;
+            echo "<br>";
+        }
+    }
 }
+
+$firstMovie = new Movie('Social Network', 'Biografia', 'en', '4');
+
+$secondMovie = new Movie('Gli Stagisti', 'Commedia', 'en', '3');
+
+$thirdMovie = new Movie('Bad Boys', 'Azione', 'en', '2');
+
+$firstMovie->all_print($firstMovie);
+
+$secondMovie->all_print($secondMovie);
+
+$thirdMovie->all_print($thirdMovie);
